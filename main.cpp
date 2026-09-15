fc5633ccd6cd9da5fb9ff9e9febb9a2fd7a35978
sabareeshwaran1302 <sabareeshwaran1302@gmail.com>
2026-09-15 23:40:23 +0530

    Initialize RapidAid project

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "======================================" << endl;
+    cout << "       RAPIDAID EMERGENCY SYSTEM     " << endl;
+    cout << "======================================" << endl;
+    cout << "System started successfully!" << endl;
+
+    return 0;
+}
